75657ac774457e748b11dd87164cad7ee41f5f12
Facundo Gonzalez <rojas.facundo2002@gmail.com>
2023-12-23 14:45:18 -0300

    Comienzo del tp2

---
+++ Tps/Tp 2/ProductManager.js
@@ -0,0 +1,30 @@
+class ProductManager {
+
+    constructor(path){
+        this.Products = []
+        this.id = 0
+        this.path = path
+    }
+
+
+    async addProduct(){
+
+    }
+
+    async getProducts(){
+
+    }
+
+    async getProductById(){
+
+    }
+
+    async updateProduct(){
+
+    }
+    
+    async deleteProduct(){
+        
+    }
+
+}
